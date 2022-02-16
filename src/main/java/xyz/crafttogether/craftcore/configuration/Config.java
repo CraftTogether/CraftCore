39113db557b9fe4ea1988fca2c894afbffa5fec4
Polaris <polariandev@protonmail.com>
2022-02-16 15:13:31 +0000

    check for null values within the config

--- src/main/java/xyz/crafttogether/craftcore/configuration/Config.java
+++ src/main/java/xyz/crafttogether/craftcore/configuration/Config.java
@@ -1,10 +1,21 @@
 package xyz.crafttogether.craftcore.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import xyz.crafttogether.craftcore.CraftCore;
+
 public class Config {
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
     private final int configVersion;
     private final String discordToken;
 
     public Config(int configVersion, String discordToken) {
+        try {
+            assert (discordToken != null);
+        } catch (AssertionError e) {
+            logger.error("Invalid CraftCore config");
+            CraftCore.unload();
+        }
         this.configVersion = configVersion;
         this.discordToken = discordToken;
     }
