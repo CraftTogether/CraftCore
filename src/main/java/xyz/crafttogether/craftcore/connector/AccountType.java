49103fdba168e02129afc7cf366b669cf9d80f4c
Polarian <polarian@polarian.dev>
2022-03-17 12:34:46 +0000

    added javadocs to AccountType

--- src/main/java/xyz/crafttogether/craftcore/connector/AccountType.java
+++ src/main/java/xyz/crafttogether/craftcore/connector/AccountType.java
@@ -4,6 +4,12 @@ package xyz.crafttogether.craftcore.connector;
  * Enum containing all the account types which are supported
  */
 public enum AccountType {
+    /**
+     * Minecraft account
+     */
     MINECRAFT,
+    /**
+     * Discord account
+     */
     DISCORD
 }
